d04d40ee5b7399170d6be82dc294fef960939227
akashkr2331 <84612937+akashkr2331@users.noreply.github.com>
2023-12-20 12:09:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ require("dotenv").config()
 const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
 const bodyParser = require("body-parser")
 const cors = require("cors")
-// const https = require("https"); 
-// const fs = require("fs");   
 const mongoose=require("mongoose")
 const User=require("./models/user")
 
@@ -20,7 +18,6 @@ mongoose.set('strictQuery', true);
 const dbUrl = process.env.dbUrl;
 mongoose.connect(dbUrl)
 
-// mongoose.connect('mongodb://127.0.0.1:27017/yogaClasses');
 
 const db=mongoose.connection;
 db.on("error",console.error.bind(console,"connectin error:"));
@@ -55,6 +52,8 @@ app.post("/payment", cors(), async (req, res)=>{
     }
 })
 
+
+// run().catch(console.dir);
 app.post('/userdata',async(req,res)=>{
     try{
         const {name,dob,mob,slot}=req.body;
@@ -129,20 +128,7 @@ res.send("data stored");
       }
 })
 
-const options = { 
-    // key: fs.readFileSync("server.key"), 
-    // cert: fs.readFileSync("server.cert"), 
-    key: "server key", 
-    cert: "server certificate", 
-  }; 
-    
-  // Creating https server by passing 
-  // options and app object 
-//   https.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.write('Hello World!');
-//     res.end();
-//   })
+
   app.listen(4000, ()=>{
     console.log("Server is running")
 })
